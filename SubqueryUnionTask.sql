9910c204c9d3174cfb06bfbe0be0dfb9e7b31f42
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-05-25 14:11:01 +0400

    Complete Part 1 : UNION Practice

--- SubqueryUnionTask.sql
+++ SubqueryUnionTask.sql
@@ -51,3 +51,33 @@ INSERT INTO Applicants VALUES
 (103, 'Aisha Al Farsi', 'aisha.f@example.com', 'Website', '2025-04-28');
 
 -----
+
+
+select * from Applicants
+select * from Trainees
+-- Part 1: UNION Practice
+-- 1. List all unique people who either trained or applied for a job.
+-- o Show their full names and emails.
+-- o Use UNION (not UNION ALL) to avoid duplicates.
+SELECT FullName, Email FROM Trainees
+UNION
+SELECT FullName, Email FROM Applicants;
+
+-- 2. Now use UNION ALL. What changes in the result?
+-- o Explain why one name appears twice.
+SELECT FullName, Email FROM Trainees
+UNION ALL
+SELECT FullName, Email FROM Applicants;
+
+ -- Layla Al Riyami appears twice in UNION ALL because she is in both tables.-- 
+ --  UNION removes duplicates; UNION ALL includes all records.-- 
+
+
+-- 3. Find people who are in both tables.
+-- o You must use INTERSECT if supported, or simulate it using INNER JOIN on Email.
+
+SELECT T.FullName, T.Email
+FROM Trainees T
+INNER JOIN Applicants A ON T.Email = A.Email;
+
+
